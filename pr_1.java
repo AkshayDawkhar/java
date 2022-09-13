1cdda7354f7f6e85e5615781620fe0531cfc5f89
Akshay Dawkhar <akshaydawkhar59@gmail.com>
2022-09-14 01:19:51 +0530

    first commit for practial 1

---
+++ pr_1.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+
+public class pr_1 {
+    public static void main(String[] args) {
+        Frame jf = new Frame();
+        jf.setSize(500,500);
+        jf.setVisible(true);
+        jf.setLayout(null);
+        CheckboxGroup ch = new CheckboxGroup();
+        String s[]={"pizza","burger","coke","chips"};
+        Checkbox []c= new Checkbox[4];
+        int x=10;
+        for (int i = 0; i < 4; i++) {
+            c[i]=new Checkbox(s[i]);
+            c[i].setBounds(x+=60,30,50,50);
+            jf.add(c[i]);
+        }
+        Checkbox ch1=new Checkbox("1",ch,false);
+        ch1.setBounds(30,60,50,50);
+        Checkbox ch2=new Checkbox("2",ch,false);
+        ch2.setBounds(30,120,50,50);
+        jf.add(ch1);
+        jf.add(ch2);
+    }
+}
